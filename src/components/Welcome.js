c1f0ff999de7554128627b2f4fc613a21971cf1e
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-07 18:06:54 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,9 @@
 // write code for Welcome component here
-function Welcome(){
-  const name="geet";
+function Welcome(prop){
+  //const name="geet";
   return(
     <>
-<h1>Hay {name}!</h1>
+<h1>Hay {prop.name}!</h1>
   <h2>welcome to newton school</h2>
 </>
 );
