00460c29001346e22eb8682631a7f153e7495abb
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-07 23:06:27 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,10 +1,10 @@
 // write code for Welcome component here
 import React from 'react'
-function Welcome(props){
+function Welcome(name){
   //const name="geet";
   return(
     <>
-<h1>Hay {props.name}!</h1>
+<h1>Hay {name.name}!</h1>
  <h2>Welcome to newton school</h2>
 </>
 );
