cb10bb19561f213616771ff833209de009980fd7
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-07 16:01:28 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,9 @@
 // write code for Welcome component here
 function Welcome(){
   //const name="geet";
+  return(
 <h1>Hay {name}!</h1>
   <h2>welcome to newton school</h2>
+);
 }
 export default Welcome;
