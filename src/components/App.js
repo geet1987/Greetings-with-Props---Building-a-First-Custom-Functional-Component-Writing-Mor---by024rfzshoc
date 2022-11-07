928a26ac7093adde52b277fea5abcc2572185777
geet1987 <52599381+geet1987@users.noreply.github.com>
2022-11-07 23:29:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,9 @@ import Welcome from './Welcome';
 
 const App = () => {
   return (
-    
     <Welcome name="geet">
-    <h2>Welcome to Newton School</h2></Welcome>
-       
+    <h2>Welcome to Newton School</h2></Welcome>     
   );
-}
-
+};
 
 export default App;
